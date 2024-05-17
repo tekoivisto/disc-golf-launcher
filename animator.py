aea2097143d2157252670858835af38ca0236696
Teemu <teemu.koivisto@tuni.fi>
2024-05-17 11:32:02 +0300

    Refactor animator to class

--- animator.py
+++ animator.py
@@ -5,108 +5,117 @@ import numpy as np
 import cv2
 
 
-def draw_launcher(launcher, history_idx, fig, lines, weight_point, rope_vertical,
-                  rubber_band, weight_x, draw_weight=True):
-    pos = launcher.pos_history[history_idx]
-    theta = launcher.theta_history[history_idx]
+class Animator:
 
-    if draw_weight:
-        weight_start_y = launcher.r[1]
-        pos_weight = launcher.pos_weight_history[history_idx]
-        rubber_band_height = launcher.rubber_band_height_history[history_idx]
-        weight_y = weight_start_y + pos_weight - launcher.h
+    def __init__(self):
 
-        if pos_weight >= rubber_band_height:
-            rope_vertical.set_data([weight_x, weight_x], [weight_start_y, weight_y])
-            rubber_band.set_data([], [])
+        self.fig, self.ax = plt.subplots(1, 1)
+        self.ax.set_xlabel('x (m)')
+        self.ax.set_ylabel('y (m)')
+        self.ax.set_aspect('equal')
 
-        else:
-            rubber_band_top_y = weight_start_y-launcher.h+rubber_band_height
-            rope_vertical.set_data([weight_x, weight_x], [weight_start_y, rubber_band_top_y])
-            rubber_band.set_data([weight_x, weight_x], [rubber_band_top_y, weight_y])
+        self.weight_point = self.ax.plot([], [], 'ko')[0]
+        rope_color = 'tab:brown'
+        self.rope_horizontal = self.ax.plot([], [], '-', color=rope_color)[0]
+        self.rope_vertical = self.ax.plot([], [], '-', color=rope_color)[0]
+        self.rubber_band = self.ax.plot([], [], '-', color='tab:green')[0]
+        self.rod_lines = None
 
-        weight_point.set_data([weight_x], [weight_y])
+        self.launcher = None
+        self.weight_x = None
+        self.fps = None
+        self.video = None
 
-    for i in range(launcher.n_joints):
-        c_mag = launcher.c_mag[i]
-        l_mag = launcher.l_mag[i]
+    def animate(self, launcher, video_fname, fps=30, lim=6, weight_x=-3):
 
-        unit_vec = np.array([np.cos(theta[i]), np.sin(theta[i])])
+        self.launcher = launcher
+        self.fps = fps
+        self.weight_x = weight_x
 
-        start = pos[i] - c_mag * unit_vec
-        end = pos[i] + (l_mag - c_mag) * unit_vec
+        n_steps = self.launcher.pos_history.shape[0]
 
-        lines[i].set_data([start[0], end[0]], [[start[1], end[1]]])
+        frame_jump = int(1 / fps / self.launcher.dt)
+        save_idx = np.array(range(0, n_steps, frame_jump))
 
-    fig.canvas.draw()
+        self.rod_lines = [self.ax.plot([], [], 'ko-')[0] for i in range(self.launcher.n_joints)]
+        self.rope_horizontal.set_data([self.weight_x, self.launcher.r[0]], [self.launcher.r[1], self.launcher.r[1]])
 
-    img = np.array(fig.canvas.renderer.buffer_rgba())
-    img = img[:, :, :-1]
+        self.ax.set_xlim([-lim, lim])
+        self.ax.set_ylim([-lim, lim])
 
-    return img
+        self.fig.canvas.draw()
+        img = self.fig.canvas.renderer.buffer_rgba()
+        height = img.shape[0]
+        width = img.shape[1]
 
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.video = cv2.VideoWriter(video_fname, fourcc, self.fps, (width, height))
 
-def draw_weight_fall(launcher, weight_point, rope_vertical, weight_x, fig, video, fps):
+        self.animate_weight_initial_fall()
 
-    t_fall = np.sqrt(2*launcher.h/g_mag)
-    t_fall_steps = np.arange(0, t_fall, 1/fps)
+        for idx in save_idx:
+            self.draw_launcher(idx)
 
-    weight_start_y = launcher.r[1]
-    for t_f in t_fall_steps:
-        weight_y = weight_start_y - 0.5 * g_mag * t_f ** 2
+        self.video.release()
 
-        rope_vertical.set_data([weight_x, weight_x], [weight_start_y, weight_y])
+    def draw_launcher(self, history_idx, draw_weight=True, save_video=True):
 
-        weight_point.set_data([weight_x], [weight_y])
+        pos = self.launcher.pos_history[history_idx]
+        theta = self.launcher.theta_history[history_idx]
 
-        fig.canvas.draw()
+        if draw_weight:
+            weight_start_y = self.launcher.r[1]
+            pos_weight = self.launcher.pos_weight_history[history_idx]
+            rubber_band_height = self.launcher.rubber_band_height_history[history_idx]
+            weight_y = weight_start_y + pos_weight - self.launcher.h
 
-        img = np.array(fig.canvas.renderer.buffer_rgba())
+            if pos_weight >= rubber_band_height:
+                self.rope_vertical.set_data([self.weight_x, self.weight_x], [weight_start_y, weight_y])
+                self.rubber_band.set_data([], [])
+
+            else:
+                rubber_band_top_y = weight_start_y - self.launcher.h + rubber_band_height
+                self.rope_vertical.set_data([self.weight_x, self.weight_x], [weight_start_y, rubber_band_top_y])
+                self.rubber_band.set_data([self.weight_x, self.weight_x], [rubber_band_top_y, weight_y])
+
+            self.weight_point.set_data([self.weight_x], [weight_y])
+
+        for i in range(self.launcher.n_joints):
+            c_mag = self.launcher.c_mag[i]
+            l_mag = self.launcher.l_mag[i]
+
+            unit_vec = np.array([np.cos(theta[i]), np.sin(theta[i])])
+
+            start = pos[i] - c_mag * unit_vec
+            end = pos[i] + (l_mag - c_mag) * unit_vec
+
+            self.rod_lines[i].set_data([start[0], end[0]], [[start[1], end[1]]])
+
+        if save_video:
+            self.canvas_to_video_frame()
+
+    def animate_weight_initial_fall(self):
+
+        self.draw_launcher(0, draw_weight=False, save_video=False)
+
+        t_fall = np.sqrt(2*self.launcher.h/g_mag)
+        t_fall_steps = np.arange(0, t_fall, 1/self.fps)
+
+        weight_start_y = self.launcher.r[1]
+        for t_f in t_fall_steps:
+            weight_y = weight_start_y - 0.5 * g_mag * t_f ** 2
+
+            self.rope_vertical.set_data([self.weight_x, self.weight_x], [weight_start_y, weight_y])
+
+            self.weight_point.set_data([self.weight_x], [weight_y])
+
+            self.canvas_to_video_frame()
+
+    def canvas_to_video_frame(self):
+        self.fig.canvas.draw()
+        img = np.array(self.fig.canvas.renderer.buffer_rgba())
         img = img[:, :, :-1]
-        video.write(img)
-
-
-def animate(launcher, fps, video_fname):
-
-    n_steps = launcher.pos_history.shape[0]
-
-    frame_jump = int(1/fps / launcher.dt)
-    save_idx = np.array(range(0, n_steps, frame_jump))
-
-    weight_x = -3
-
-    fig, ax = plt.subplots(1, 1)
-    lines = [ax.plot([], [], 'ko-')[0] for i in range(launcher.n_joints)]
-    weight_point = ax.plot([], [], 'ko')[0]
-    rope_color = 'tab:brown'
-    rope_horizontal = ax.plot([weight_x, launcher.r[0]], [launcher.r[1], launcher.r[1]], '-', color=rope_color)[0]
-    rope_vertical = ax.plot([], [], '-', color=rope_color)[0]
-    rubber_band = ax.plot([], [], '-', color='tab:green')[0]
-
-    lim = 6
-    ax.set_xlabel('x (m)')
-    ax.set_ylabel('y (m)')
-    ax.set_xlim([-lim, lim])
-    ax.set_ylim([-lim, lim])
-    ax.set_aspect('equal')
-
-    fig.canvas.draw()
-
-    img = fig.canvas.renderer.buffer_rgba()
-    height = img.shape[0]
-    width = img.shape[1]
-
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video = cv2.VideoWriter(video_fname, fourcc, fps, (width, height))
-
-    draw_launcher(launcher, 0, fig, lines, weight_point, rope_vertical, rubber_band, weight_x, False)
-    draw_weight_fall(launcher, weight_point, rope_vertical, weight_x, fig, video, fps)
-
-    for idx in save_idx:
-        img = draw_launcher(launcher, idx, fig, lines, weight_point, rope_vertical, rubber_band, weight_x)
-        video.write(img)
-
-    video.release()
+        self.video.write(img)
 
 
 def main():
@@ -125,16 +134,14 @@ def main():
 
     dt = 0.001
     T = 5
-    fps = 30
 
     launcher = Launcher(theta, l_mag, c_mag, m, I, r_mag, m_weight, h, k, omega)
     launcher.simulate(dt, T)
 
     print('simulation done, generating video')
 
-    animate(launcher, fps, 'animations/video.mp4')
-
-    plt.show()
+    animator = Animator()
+    animator.animate(launcher, 'animations/video.mp4')
 
 
 if __name__ == '__main__':
